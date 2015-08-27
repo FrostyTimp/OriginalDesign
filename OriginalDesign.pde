cce0efa71bdbe9f9e5316131a8e5c9e82254fcb3
FrostyTimp <axu210@gmail.com>
2015-08-27 07:43:24 -0700

    Made bouncing ball with block

--- OriginalDesign.pde
+++ OriginalDesign.pde
@@ -1,8 +1,8 @@
 //1 is true, 2 is false
-float x=random(26, 374);
-float y=random(26, 374);
-int moveRight=1;
-int moveDown=1;
+int x=int(random(41, 359));
+int y=int(random(41, 359));
+boolean moveRight=false;
+boolean moveDown=false;
 void setup()
 {
   size(400, 400);
@@ -11,30 +11,109 @@ void setup()
 }
 void draw()
 {
+  background(0);
+  block();
   ball();
+  border();
 }
 void ball()
 {
-  background(0);
-  float r=random(0, 255);
-  float g=random(0, 255);
-  float b=random(0, 255);
+  float r=int(random(0, 255));
+  float g=int(random(0, 255));
+  float b=int(random(0, 255));
+  stroke(0);
+  strokeWeight(1);
   fill(r, g, b);
   ellipse(x, y, 50, 50);
-  if (x<26)
-    moveRight=1;
-  if (x>374)
-    moveRight=2;
-  if (y<26)
-    moveDown=1;
-  if (y>374)
-    moveDown=2;
-  if (moveRight<2)
-    x++;
-  if (moveRight>1)
-    x--;
-  if (moveDown<2)
-    y++;
-  if (moveDown>1)
-    y--;
+  if(get(x+31,y)!=color(0))
+  	moveRight=false;
+  if(get(x-31,y)!=color(0))
+  	moveRight=true;
+  if(get(x,y+31)!=color(0))
+  	moveDown=false;
+  if(get(x,y-31)!=color(0))
+  	moveDown=true;
+  if(!moveRight)
+  {
+  	x--;
+  }
+  else
+  {
+  	x++; 
+  }
+  if(!moveDown)
+  {
+  	 y--;
+  }
+  else 
+  {
+  	 y++;
+  }
 }
+void block()
+{
+	stroke(255);
+	fill(255);
+	rect(mouseX-10,mouseY-10,20,20);
+}
+void border()
+{
+	stroke(255);
+  strokeWeight(20);
+  noFill();
+  rect(0,0,400,400);
+}
+
+
+
+
+
+
+
+
+
+
+
+
+// void setup()
+// {
+// 	size(270,270);
+// 	background(96);
+// }
+// void draw()
+// {
+// 	goomy();
+// 	ears();
+// 	shade();
+// }
+// void goomy()
+// {
+// 	stroke(32);
+// 	strokeWeight(7);
+// 	fill(229,204,255);
+// 	//body
+// 	ellipse(120,150,200,190);
+// 	//eyes
+// 	ellipse(78,132,6,12);
+// 	ellipse(130,133,6,12);
+// 	//ears
+// 	// beginShape();
+// 	// curveVertex(103,123);
+// 	// curveVertex(48,78);
+// 	// curveVertex(38,52);
+// 	// curveVertex(52,48);
+// 	// endShape();
+// }
+// void ears()
+// {
+// 	beginShape();
+// 	curveVertex(103,123);
+// 	curveVertex(48,78);
+// 	curveVertex(38,52);
+// 	curveVertex(52,48);
+// 	endShape();
+// }
+// void shade()
+// {
+
+// }
--- build-tmp/source/OriginalDesign.java
+++ build-tmp/source/OriginalDesign.java
@@ -15,10 +15,10 @@ import java.io.IOException;
 public class OriginalDesign extends PApplet {
 
 //1 is true, 2 is false
-float x=random(26, 374);
-float y=random(26, 374);
-int moveRight=1;
-int moveDown=1;
+int x=PApplet.parseInt(random(41, 359));
+int y=PApplet.parseInt(random(41, 359));
+boolean moveRight=false;
+boolean moveDown=false;
 public void setup()
 {
   size(400, 400);
@@ -27,33 +27,112 @@ public void setup()
 }
 public void draw()
 {
+  background(0);
+  block();
   ball();
+  border();
 }
 public void ball()
 {
-  background(0);
-  float r=random(0, 255);
-  float g=random(0, 255);
-  float b=random(0, 255);
+  float r=PApplet.parseInt(random(0, 255));
+  float g=PApplet.parseInt(random(0, 255));
+  float b=PApplet.parseInt(random(0, 255));
+  stroke(0);
+  strokeWeight(1);
   fill(r, g, b);
   ellipse(x, y, 50, 50);
-  if (x<26)
-    moveRight=1;
-  if (x>374)
-    moveRight=2;
-  if (y<26)
-    moveDown=1;
-  if (y>374)
-    moveDown=2;
-  if (moveRight<2)
-    x++;
-  if (moveRight>1)
-    x--;
-  if (moveDown<2)
-    y++;
-  if (moveDown>1)
-    y--;
+  if(get(x+31,y)!=color(0))
+  	moveRight=false;
+  if(get(x-31,y)!=color(0))
+  	moveRight=true;
+  if(get(x,y+31)!=color(0))
+  	moveDown=false;
+  if(get(x,y-31)!=color(0))
+  	moveDown=true;
+  if(!moveRight)
+  {
+  	x--;
+  }
+  else
+  {
+  	x++; 
+  }
+  if(!moveDown)
+  {
+  	 y--;
+  }
+  else 
+  {
+  	 y++;
+  }
 }
+public void block()
+{
+	stroke(255);
+	fill(255);
+	rect(mouseX-10,mouseY-10,20,20);
+}
+public void border()
+{
+	stroke(255);
+  strokeWeight(20);
+  noFill();
+  rect(0,0,400,400);
+}
+
+
+
+
+
+
+
+
+
+
+
+
+// void setup()
+// {
+// 	size(270,270);
+// 	background(96);
+// }
+// void draw()
+// {
+// 	goomy();
+// 	ears();
+// 	shade();
+// }
+// void goomy()
+// {
+// 	stroke(32);
+// 	strokeWeight(7);
+// 	fill(229,204,255);
+// 	//body
+// 	ellipse(120,150,200,190);
+// 	//eyes
+// 	ellipse(78,132,6,12);
+// 	ellipse(130,133,6,12);
+// 	//ears
+// 	// beginShape();
+// 	// curveVertex(103,123);
+// 	// curveVertex(48,78);
+// 	// curveVertex(38,52);
+// 	// curveVertex(52,48);
+// 	// endShape();
+// }
+// void ears()
+// {
+// 	beginShape();
+// 	curveVertex(103,123);
+// 	curveVertex(48,78);
+// 	curveVertex(38,52);
+// 	curveVertex(52,48);
+// 	endShape();
+// }
+// void shade()
+// {
+
+// }
   static public void main(String[] passedArgs) {
     String[] appletArgs = new String[] { "OriginalDesign" };
     if (passedArgs != null) {
